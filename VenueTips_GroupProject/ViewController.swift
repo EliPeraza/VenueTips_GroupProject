e2a155bc60a368d4531dd97b39d95c58614ea3be
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-02-08 12:57:58 -0500

    first test for master

---
+++ VenueTips_GroupProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  VenueTips_GroupProject
+//
+//  Created by Elizabeth Peraza  on 2/8/19.
+//  Copyright © 2019 Elizabeth Peraza . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    print("first test")
+
+  }
+
+
+}
+
