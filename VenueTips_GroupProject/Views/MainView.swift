6c546c6619a967f7383bff2fb12e6d1f9225973f
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-02-11 12:37:11 -0500

    Added main view controller and updated the model with Leandros info

---
+++ VenueTips_GroupProject/Views/MainView.swift
@@ -0,0 +1,15 @@
+//
+//  MainView.swift
+//  VenueTips_GroupProject
+//
+//  Created by Elizabeth Peraza  on 2/11/19.
+//  Copyright © 2019 Elizabeth Peraza . All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+ 
+
+}
