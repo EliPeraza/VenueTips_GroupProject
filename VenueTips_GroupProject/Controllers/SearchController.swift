681cbf5961854a539ce7ad41a375aca4b073e301
Destrella3 <destrella385@gmail.com>
2019-02-11 12:09:49 -0500

    searchView setup done

--- VenueTips_GroupProject/Controllers/SearchController.swift
+++ VenueTips_GroupProject/Controllers/SearchController.swift
@@ -10,21 +10,33 @@ import UIKit
 
 class SearchController: UIViewController {
 
+    let searchView = SearchView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(searchView)
+        searchView.searchTableView.dataSource = self
+        searchView.searchTableView.delegate = self
+        searchView.searchTableView.register(UITableViewCell.self, forCellReuseIdentifier: "SearchCell")
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+extension SearchController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = searchView.searchTableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath)
+        cell.textLabel?.text = "Searched Venue"
+    
+        return cell
     }
-    */
+}
 
+extension SearchController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
