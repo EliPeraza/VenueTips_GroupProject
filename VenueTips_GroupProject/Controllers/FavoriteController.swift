61647aa42d3831d99d6ddd652a44ae54fa98a6fc
Destrella3 <destrella385@gmail.com>
2019-02-15 15:59:00 -0500

    data persistence working

--- VenueTips_GroupProject/Controllers/FavoriteController.swift
+++ VenueTips_GroupProject/Controllers/FavoriteController.swift
@@ -11,14 +11,12 @@ import UIKit
 class FavoriteController: UIViewController {
   
   let favoriteView = FavoriteView()
-
     let thisTest = "Test"
   
-  var categoriesTest = [CategoriesDetails]() {
+  var categories = DataManager.getCategories(fileName: DataManager.categoriesFileName) {
     didSet {
       DispatchQueue.main.async {
         self.favoriteView.favoriteCollectionView.reloadData()
-        print("These are the number of categories: \(self.categoriesTest.count)")
 
       }
     }
@@ -37,26 +35,42 @@ class FavoriteController: UIViewController {
     let addCategoryButton = UIBarButtonItem(title: "New Category", style: UIBarButtonItem.Style.plain, target: self, action: #selector(addCategoryButtonPressed))
     self.navigationItem.rightBarButtonItem = addCategoryButton
     
-    getCategories()
+//    getCategories()
     
   }
   
   @objc private func addCategoryButtonPressed() {
-    
+    showAlert()
   }
   
-  func getCategories() {
-    CategoriesAPIClient.getCategories(location: "40.7,-74", date: "20190208") { (appError, categories) in
-      if let appError = appError {
-        print(appError)
-      }
-      if let categoriesData = categories {
-       self.categoriesTest = categoriesData
-        dump(self.categoriesTest)
-      }
+//  func getCategories() {
+//    CategoriesAPIClient.getCategories(location: "40.7,-74", date: "20190208") { (appError, categories) in
+//      if let appError = appError {
+//        print(appError)
+//      }
+//      if let categoriesData = categories {
+//       self.categoriesTest = categoriesData
+//        dump(self.categoriesTest)
+//      }
+//    }
+//  }
+  
+    func showAlert() {
+        let alert = UIAlertController(title: "Enter Category Name", message: nil, preferredStyle: .alert)
+        alert.addTextField { (texField) in
+            texField.text = "E.g Tacos or Yoga"
+        }
+        let ok = UIAlertAction(title: "Ok", style: .default) { (done) in
+            if let categoryName =  alert.textFields?.first?.text {
+                let categoryToSave = CategoryToSave.init(categoryName: categoryName)
+                DataManager.addCatergory(fileName: DataManager.categoriesFileName, category: categoryToSave)
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
     }
-  }
-  
   
   
 }
@@ -64,16 +78,16 @@ class FavoriteController: UIViewController {
 
 extension FavoriteController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return categoriesTest.count
+    return categories.count
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     
     guard let cell = self.favoriteView.favoriteCollectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteCollectionCell", for: indexPath) as? FavoriteCollectionCell else {return UICollectionViewCell()}
     
-    let currentCategory = categoriesTest[indexPath.row]
+    let currentCategory = categories[indexPath.row]
     
-    cell.title.text = currentCategory.name
+    cell.title.text = currentCategory.categoryName
     cell.backgroundColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
     return cell
     
@@ -84,5 +98,7 @@ extension FavoriteController: UICollectionViewDelegateFlowLayout, UICollectionVi
     return CGSize.init(width: 120, height: 120)
     
   }
-  
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(categories[indexPath.row].categoryName)
+    }
 }
--- VenueTips_GroupProject/FileManagerHelpers/DataManager.swift
+++ VenueTips_GroupProject/FileManagerHelpers/DataManager.swift
@@ -8,3 +8,75 @@
 
 import Foundation
 
+struct DataManager {
+   static private var categories = [CategoryToSave]()
+   static private var venues = [Venue]()
+   static var categoriesFileName = "categoriesFileName.plist"
+    
+    static func saveCategory(fileName: String) {
+        let path = DataPersistenceManager.filepathToDocumentsDirectory(filename: fileName)
+        do {
+            let data = try PropertyListEncoder().encode(categories)
+            try data.write(to: path, options: .atomic)
+        } catch {
+            print("Property List and Encoding Error: \(error)")
+        }
+    }
+    
+    static func saveVenues(venueName: String) {
+        let path = DataPersistenceManager.filepathToDocumentsDirectory(filename: venueName)
+        do {
+            let data = try PropertyListEncoder().encode(venues)
+            try data.write(to: path, options: .atomic)
+        } catch {
+            print("Property List and Encoding Error: \(error)")
+        }
+    }
+    
+    static func addCatergory(fileName: String, category: CategoryToSave) {
+        categories.append(category)
+        saveCategory(fileName: fileName)
+    }
+    
+    static func addVenue(categoryName: String, venue: Venue) {
+        venues.append(venue)
+        saveVenues(venueName: categoryName)
+    }
+    
+    static func getCategories(fileName: String) -> [CategoryToSave] {
+        let path = DataPersistenceManager.filepathToDocumentsDirectory(filename: fileName).path
+        if FileManager.default.fileExists(atPath: path) {
+            if let data = FileManager.default.contents(atPath: path) {
+                do {
+                    categories = try PropertyListDecoder().decode([CategoryToSave].self, from: data)
+                } catch {
+                    print("Propery List Decoding Error: \(error)")
+                }
+            } else {
+                print("Categories Data is Nil")
+            }
+        } else {
+            print("\(fileName) does not Exist")
+        }
+        return categories
+    }
+    
+    static func getVenues(categoryName: String) -> [Venue] {
+        let path = DataPersistenceManager.filepathToDocumentsDirectory(filename: categoryName).path
+        if FileManager.default.fileExists(atPath: path) {
+            if let data = FileManager.default.contents(atPath: path) {
+                do {
+                    venues = try PropertyListDecoder().decode([Venue].self, from: data)
+                } catch {
+                    print("Propery List Decoding Error: \(error)")
+                }
+            } else {
+                print("Categories Data is Nil")
+            }
+        } else {
+            print("\(categoryName) does not Exist")
+        }
+        return venues
+    }
+    
+}
